cfd2f4f6e125a1b6aa4607f7104208d9e16b7017
Aliaksandr Shyrko <Al.shyrko@gmail.com>
2016-11-09 16:21:24 +0300

    update user table

---
+++ database/migrations/2016_11_09_093101_update_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function ($table) {
+            $table->enum('status', ['active', 'inactive'])->default('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
